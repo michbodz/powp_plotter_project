635283c0598d3da6c73b1a4f5d4b77642fb1dfc6
Michał Bodzioch <michbodz@gmail.com>
2017-04-27 15:18:59 +0200

    3.1.2

---
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,31 @@
+package edu.iis.powp.command;
+
+import java.util.List;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class ComplexCommand implements PlotterCommand {
+	
+	private List<PlotterCommand> list;
+	
+	
+	public ComplexCommand(List<PlotterCommand> list){
+		super();
+		this.list = list;
+	}
+	
+	@Override
+	public void execute(IPlotter driver) {
+		for (Object command : list) {
+			((PlotterCommand)command).execute(driver);
+		}
+	}
+	
+	public List<PlotterCommand> getList() {
+		return list;
+	}
+	
+	public void setList(List<PlotterCommand> list) {
+		this.list = list;
+	}
+}
